3d60df40c53a002259f98621c4747f2110b0fa1d
Tuan Dinh Luong <tdluong@crimson.ua.edu>
2024-11-20 08:47:23 -0600

    Typo in order of quat multiplication for sensor pose calc

--- util/get_sensor_pose.m
+++ util/get_sensor_pose.m
@@ -1,4 +1,4 @@
 function [sensor_pos, sensor_quat] = get_sensor_pose(base_pos, base_quat, sensor_params)
     sensor_pos = base_pos + transpose(rotatepoint(base_quat,sensor_params.pos_body_sensor'));
-    sensor_quat = quatmultiply(sensor_params.quat_body_sensor, base_quat);
+    sensor_quat = quatmultiply(base_quat, sensor_params.quat_body_sensor);
 end
